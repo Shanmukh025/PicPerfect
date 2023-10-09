de2ff7ea17aa620c55df08e8cca6381297b4b09c
T.Phaneendra Nikhil <tallapragadaphaneendranikhil@gmail.com>
2023-10-09 14:40:15 +0530

    navbar create

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Pages/navbar'
+import Header from './Pages/header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Header />
     </>
   )
 }
--- src/Pages/navbar.jsx
+++ src/Pages/navbar.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
-
+import logo from '../images/logo.jpg'
 const Navbar = () => {
   return (
     <>
         <nav>
-            <div className="search-bar">
-            /*hello*/
-            </div>
+
+            <div className="nav-container-1">
             <div className="nav-logo">
-
+              {/* <img src={logo} alt="" /> */}
+              <h1>Pic<span>Perfect</span></h1>
             </div>
+            
             <div className="hero-img">
+              
+              <button className='btn-primary'><a href="#">join</a></button>
+              <button className='btn-primary'><a href="#">Login</a></button>
+              <button className='upload-btn'><a href="#">Upload</a></button>
 
             </div>
+            </div>
+
+            <div className="nav-container-2">
+              
+              <div className="search-bar">
+                <input type="search" placeholder='search PicPerfect' required/>
+              </div>
+            </div>
+
         </nav>
     </>
   )
