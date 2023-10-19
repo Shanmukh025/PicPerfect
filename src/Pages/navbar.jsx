78f34304d67dba3691c9da131a616b1d72a6fd31
Shanmukh025 <shanmukh2564@gmail.com>
2023-10-19 18:39:30 +0530

    commit

--- src/Pages/navbar.jsx
+++ src/Pages/navbar.jsx
@@ -4,31 +4,31 @@ import logo from "../images/logo.png";
 const Navbar = () => {
   return (
     <>
-        <nav>
+      <nav>
+        <div className="nav-container-1">
+          <div className="nav-logo">
+            <img src={logo} alt="" width="150" height="150" />
+          </div>
 
-            <div className="nav-container-1">
-            <div className="nav-logo">
-              {<img src={logo} alt="" width="150" height="150"/> }
-            </div>
-            
-            <div className="hero-img">
-              <button className='btn-primary'><a href="#">Join</a></button>
-              <button className='btn-primary'><a href="#">Login</a></button>
-              <button class="btn upload-btn">
-    <label for="imageInput" class="upload-label">Upload</label>
-  </button></div>
-            </div>
+          <div className="hero-img">
+            <button className="btn-primary"><a href="#">Join</a></button>
+            <button className="btn-primary"><a href="#">Login</a></button>
+            <button className="btn upload-btn">
+              <label htmlFor="imageInput" className="upload-label">Upload</label>
+            </button>
+          </div>
+        </div>
 
-            <div className="nav-container-2">
-              
-              <div className="search-bar">
-                <input type="search" placeholder='Search PicPerfect' required/>
-              </div>
-            </div>
-
-        </nav>
+        <div className="nav-container-2">
+        <div class="container">
+          <div class="container">
+            <input type="text" name="text" class="input" placeholder="Search PicPerfect"/>
+          </div>
+        </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
