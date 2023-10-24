a95fe9d31d9693d7a7406651db718e56406e0881
Shanmukh025 <shanmukh2564@gmail.com>
2023-10-24 12:33:58 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Home from './Pages/home';
+import Home from './Pages/Home';
 
 function App() {
   return (
--- src/Pages/navbar.jsx
+++ src/Pages/navbar.jsx
@@ -8,12 +8,12 @@ function Navbar() {
         <nav>
         <div className="nav-container-1">
           <div className="nav-logo">
-            <img src={logo} alt="" width="150" height="150" />
+            <img src={logo} width="150" height="150" />
           </div>
 
           <div className="hero-img">
-            <button className="btn-primary"><h4>JOIN</h4></button>
-            <button className="btn-primary"><h4>LOGIN</h4></button>
+            <button className="btn-primary"><h3>Join</h3></button>
+            <button className="btn-primary"><h3>Login</h3></button>
             <button className="btn upload-btn"><h3>UPLOAD</h3></button>
           </div>
         </div>
