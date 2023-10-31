762d3b725ecd0c1e8132aa181ce7681818f9db50
Shanmukh025 <shanmukh2564@gmail.com>
2023-10-31 11:53:56 +0530

    commit

--- controller/userController.js
+++ controller/userController.js
@@ -1,18 +1,19 @@
 //@desc Get all users
 //route GET /api/users
 //@access public
-const getUsers = (req, res) => {
+const getUsers = async(req, res) => {
     res.status(200).json({message: "Get all users"});
 };
 
 //@desc Create new users
 //route POST /api/users
 //@access public
-const createUser = (req, res) => {
+const createUser = async(req, res) => {
     console.log("Request body is: ", req.body);
     const {name, email, phone} = req.body;
     if(!name || !email || !phone){
-        res.status(400).json({message: "All fields are mandatory"});
+        res.status(400);
+        throw new Error("All fields are mandatory");
     }
     res.status(201).json({message: "Create user"});
 };
@@ -20,21 +21,21 @@ const createUser = (req, res) => {
 //@desc Get user
 //route POST /api/users/:id
 //@access public
-const getUser = (req, res) => {
+const getUser = async(req, res) => {
     res.status(200).json({message: `Get user for ${req.params.id}`});
 };
 
 //@desc Update user
 //route PUT /api/users/:id
 //@access public
-const updateUser = (req, res) => {
+const updateUser = async(req, res) => {
     res.status(200).json({message: `Update user for ${req.params.id}`});
 };
 
 //@desc Delete user
 //route DELETE /api/users/:id
 //@access public
-const deleteUser = (req, res) => {
+const deleteUser = async(req, res) => {
     res.status(200).json({message: `Delete user for ${req.params.id}`});
 };
 
