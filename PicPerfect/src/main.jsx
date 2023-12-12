fc6a87bf2cb2f533c55b888756c7e939b2eca15b
T.Phaneendra Nikhil <tallapragadaphaneendranikhil@gmail.com>
2023-12-12 10:01:04 +0530

    commit

--- PicPerfect/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
